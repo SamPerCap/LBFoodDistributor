f5e6e6fd23e82a199f3e5cc4f8b799c8575d72fc
SamPerCap <31726611+SamPerCap@users.noreply.github.com>
2020-02-28 14:46:50 +0100

    Add Interface but not implemented

--- LBFoodDistributor/EuropeanCRUD.cs
+++ LBFoodDistributor/EuropeanCRUD.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using LBFoodDistributor.Model;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,5 +7,19 @@ namespace LBFoodDistributor
 {
     class EuropeanCRUD : ICRUD
     {
+        public IEnumerable<Food> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Food> GetByContinent(string Continent)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Food> GetByCountry(string Country)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
