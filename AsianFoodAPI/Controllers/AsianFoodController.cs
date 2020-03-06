1645e64c01f4eaedc75b85c5bb14b6822d28c4e0
Kristófer a <andersen19@gmail.com>
2020-03-06 12:51:42 +0100

    asianfoodapi

---
+++ AsianFoodAPI/Controllers/AsianFoodController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AsianFoodAPI.Data;
+using AsianFoodAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AsianFoodAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AsianFoodController : Controller
+    {
+        private readonly IRepository<AsianFood> _asianFoodRepo;
+        public AsianFoodController(IRepository<AsianFood> asianFoodRepo)
+        {
+            _asianFoodRepo = asianFoodRepo;
+        }
+
+        // GET: api/<controller>
+        [HttpGet]
+        public ActionResult<IEnumerable<AsianFood>> Get()
+        {
+            return Ok(_asianFoodRepo.GetAll().ToList());
+        }
+    }
+}
