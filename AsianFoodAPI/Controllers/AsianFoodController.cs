6ce450ec91d7de0e3e834d04f42b582923a95e6a
Kristófer a <andersen19@gmail.com>
2020-03-06 11:44:24 +0100

    Created an API for asian food
    everything should work
    
    hopefully

---
+++ AsianFoodAPI/Controllers/AsianFoodController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AsianFoodAPI.Data;
+using AsianFoodAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AsianFoodAPI.Controllers
+{
+    public class AsianFoodController : Controller
+    {
+        private readonly IAsianFoodRepository _asianFoodRepo;
+        public AsianFoodController(IAsianFoodRepository asianFoodRepo)
+        {
+            _asianFoodRepo = asianFoodRepo;
+        }
+
+        // GET: api/<controller>
+        [HttpGet]
+        public ActionResult<IEnumerable<AsianFood>> Get()
+        {
+            return Ok(_asianFoodRepo.GetAllFood().ToList());
+        }
+    }
+}
---
+++ AsianFoodAPI/Models/AsianFood.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AsianFoodAPI.Models
+{
+    public class AsianFood
+    {
+        public string Name { get; set; }
+        public IList<string> Ingredients { get; set; }
+        public string Country { get; set; }
+        public string Continent { get; set; }
+    }
+}
