1bf24718878cf865442326f9855ecf7f741f875a
Kristófer a <andersen19@gmail.com>
2020-03-06 13:14:49 +0100

    asianapi

---
+++ AsianFoodAPI/Data/DBInitializer.cs
@@ -0,0 +1,42 @@
+﻿using AsianFoodAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AsianFoodAPI.Data
+{
+    public class DBInitializer : IDBInitializer
+    {
+        public void Initialize(AsianFoodContext context)
+        {
+            context.Database.EnsureDeleted();
+            context.Database.EnsureCreated();
+
+            // Look for any Orders
+            if (context.AsianFood.Any())
+            {
+                return;   // DB has been seeded
+            }
+
+            List<AsianFood> food = new List<AsianFood>
+            {
+                new AsianFood {
+                    Id = 1,
+                    Name = "DonkeyKong Dish",
+                    Continent = "Asia",
+                    Country = "Japan",
+                   // Ingredients = new List<string>()
+                    //{
+                    //    "Sam Souce",
+                    //    "Amazing Sam Pineapple",
+                    //    "Crazy Sam BBQ"
+                    //}
+                }
+            };
+
+            context.AsianFood.AddRange(food);
+            context.SaveChanges();
+        }
+    }
+}
