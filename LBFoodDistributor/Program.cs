e990a15785bcb6e5251387cfbd746edeb33cee7a
SamPerCap <31726611+SamPerCap@users.noreply.github.com>
2020-03-09 16:00:49 +0100

    array

--- LBFoodDistributor/Program.cs
+++ LBFoodDistributor/Program.cs
@@ -12,10 +12,15 @@ namespace LBFoodDistributor
     {
         static void Main(string[] args)
         {
-            Queue<string> asianQueue = new Queue<string>();
-            asianQueue.Enqueue("http://localhost:54774/asianfood");
-            asianQueue.Enqueue("http://localhost:54775/asianfood");
-            asianQueue.Enqueue("http://localhost:54776/asianfood");
+            string[] asianQueue = new string[3];
+            asianQueue[0] = "http://localhost:54774/asianfood";
+            asianQueue[1] = "http://localhost:54775/asianfood";
+            asianQueue[2] = "http://localhost:54776/asianfood";
+
+
+            string[] europeQueue = new string[2];
+            europeQueue[0] = "http://localhost:56326/api/EFood";
+            europeQueue[0] = "http://localhost:56327/api/EFood";
 
             string continent = "start";
 
@@ -26,12 +31,23 @@ namespace LBFoodDistributor
 
                 if (continent.ToLower().Equals("asia"))
                 {
-                    GetInfo("http://localhost:54774/api/asianFood");
-                    //GetInfo(asianQueue.Peek());
+                    for (int i = 0; i <= asianQueue.Length; i++)
+                    {
+                        if (i == 3)
+                            i = 0;
+                        else
+                            GetInfo(asianQueue[i]);
+                    }
                 }
                 else if (continent.ToLower().Equals("europe"))
                 {
-                    GetInfo("http://localhost:56326/api/EFood");
+                    for (int i = 0; i <= europeQueue.Length; i++)
+                    {
+                        if (i == 2)
+                            i = 0;
+                        else
+                            GetInfo(europeQueue[i]);
+                    }
                 }
                 else
                 {
