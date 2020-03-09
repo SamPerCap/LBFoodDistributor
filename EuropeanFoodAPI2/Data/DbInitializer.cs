3d45243c93b3dd7f414aefd0dd1448f7c265fd57
SamPerCap <31726611+SamPerCap@users.noreply.github.com>
2020-03-09 14:02:54 +0100

    EAPI2

---
+++ EuropeanFoodAPI2/Data/DbInitializer.cs
@@ -0,0 +1,64 @@
+﻿using EuropeanFoodAPI.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EuropeanFoodAPI.Data
+{
+    public class DbInitializer : IDbInitializer
+    {
+        public void Initialize(EFoodApiContext context)
+        {
+            context.Database.EnsureDeleted();
+            context.Database.EnsureCreated();
+
+            if (context.EuropeanFood.Any())
+            {
+                return;
+            }
+
+            List<Food> foodList = new List<Food>
+            {
+                new Food
+            {
+                Name = "Paella",
+                Country = "Spain",
+                Continent = "Europe",
+                MainIngredient = "Rice"
+            },
+            new Food
+            {
+                Name = "Pizza",
+                Country = "Italy",
+                Continent = "Europe",
+                MainIngredient = "Tomato"
+            },
+            new Food
+            {
+                Name = "Hakarl",
+                Country = "Island",
+                Continent = "Europe",
+                MainIngredient = "Shark"
+            },
+            new Food
+            {
+                Name = "Quiche Lorraine",
+                Country = "France",
+                Continent = "Europe",
+                MainIngredient = "Eggs"
+            },
+            new Food
+            {
+                Name = "Money",
+                Country = "Netherlands",
+                Continent ="Europe",
+                MainIngredient = "Spices"
+            }
+            };
+
+            context.EuropeanFood.AddRange(foodList);
+            context.SaveChanges();
+        }
+    }
+}
---
+++ EuropeanFoodAPI2/Data/EFoodRepository.cs
@@ -0,0 +1,27 @@
+﻿using EuropeanFoodAPI.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace EuropeanFoodAPI.Data
+{
+    public class EFoodRepository : IRepository<Food>
+    {
+        private readonly EFoodApiContext context;
+
+        public EFoodRepository(EFoodApiContext ctx)
+        {
+            context = ctx;
+        }
+
+        IEnumerable<Food> IRepository<Food>.GetAll()
+        {
+            return context.EuropeanFood.ToList();
+        }
+
+        public Food GetBycountry(string country)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
